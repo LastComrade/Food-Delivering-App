42dfc9aca58661fd44e41304ce6a168a2626aa93
Konark Lohat <konarlohat123456@gmail.com>
2021-04-08 18:44:13 +0530

    Did some styling and added login-register pages in auth folder

--- server.js
+++ server.js
@@ -11,16 +11,28 @@ const favicon = require('serve-favicon');
 app.use(express.static('public'))
 app.use(favicon(path.join(__dirname,'public','favicon.ico')))
 
+// Set template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-// Set template engine
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
+
 
 
 const port = process.env.PORT || 3000
